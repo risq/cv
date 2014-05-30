e33b5892b03c1d8d2af186408b1553e313f2cd02
risq <valentin.ledrapier@gmail.com>
2014-05-30 04:52:25 +0200

    update

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,8 @@
 ;(function(){
 
-    // Menu settings
-    $('#menuToggle, .menu-close').on('click', function(){
-        $('#menuToggle').toggleClass('active');
-        $('body').toggleClass('body-push-toleft');
-        $('#theMenu').toggleClass('menu-open');
+    $('.navbar').headroom({
+        "tolerance": 20,
+        "offset": 450
     });
 
     if($('#main-portfolio')) {
@@ -16,5 +14,19 @@
         });
     }
 
+    $('body').scrollspy({ target: '.categories' });
+
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top + 30)
+                }, 500);
+                return false;
+            }
+        }
+    });
 
 })(jQuery)
